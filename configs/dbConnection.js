f4f00b789d8528e009d84d60ea4688e76da8f2fb
Oscar Naranjo <oscarnrnj@gmail.com>
2024-11-06 18:29:38 +0100

    127.0.0.1

--- configs/dbConnection.js
+++ configs/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //const connetDB = mongoose.connect(process.env.CONN_STR + '/test')
-const connetDB = mongoose.connect('mongodb://100.20.92.101:27017/test')
+const connetDB = mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => console.log('Connected!!!'))
   .catch((e)=>{
     console.log('Database no conected', e)
